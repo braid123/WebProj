c2eb431dd45ca7da8256dd25c52b25231723f0c9
braid123 <603505420@qq.com>
2018-02-12 15:33:38 +0800

    init commit

--- v2yeah/blogweb/urls.py
+++ v2yeah/blogweb/urls.py
@@ -1,7 +1,7 @@
-from django.conf.urls import url, include
+from django.conf.urls import url
 from .views import *
 
 
 urlpatterns = [
-    # url(r'^blog/', index, name='index'),
+    url(r'^$', index, name='index'),
 ]
